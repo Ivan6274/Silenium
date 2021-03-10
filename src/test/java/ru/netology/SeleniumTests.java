8d4e8208c8e7192580f677de466a177d0e0601cb
Ivan6274 <isivanov2011@gmail.com>
2021-03-10 12:38:49 +0300

    Add Invanid Tests

--- src/test/java/ru/netology/SeleniumTests.java
+++ src/test/java/ru/netology/SeleniumTests.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -42,6 +45,37 @@ public class SeleniumTests {
 
         String actualMessage = driver.findElement(By.cssSelector(".paragraph_theme_alfa-on-white")).getText().trim();
         String expectedMessege = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expectedMessege,actualMessage);
+    }
+
+
+    @Test
+    void shouldTestOrderCartInvalidName() {
+
+        driver.get("http://localhost:7777");
+        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Alesha Popovich");
+        driver.findElement(By.cssSelector("input[type='tel']")).sendKeys("+79990000000");
+        driver.findElement(By.cssSelector("[class='checkbox__box']")).click();
+        driver.findElement(By.cssSelector("[role=button]")).click();
+        List<WebElement> elements = driver.findElements(By.cssSelector(".input__sub"));
+
+        String actualMessage = elements.get(0).getText().trim();
+        String expectedMessege = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        assertEquals(expectedMessege,actualMessage);
+    }
+
+    @Test
+    void shouldTestOrderCartInvalidPhone() {
+
+        driver.get("http://localhost:7777");
+        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Алеша Попович");
+        driver.findElement(By.cssSelector("input[type='tel']")).sendKeys("+799900000000000");
+        driver.findElement(By.cssSelector("[class='checkbox__box']")).click();
+        driver.findElement(By.cssSelector("[role=button]")).click();
+        List<WebElement> elements = driver.findElements(By.cssSelector(".input__sub"));
+
+        String actualMessage = elements.get(1).getText().trim();
+        String expectedMessege = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
         assertEquals(expectedMessege,actualMessage);
     }
 }
