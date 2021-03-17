6a62b4683fc8b829f586d2e5977eb95bce61df4c
Ivan6274 <isivanov2011@gmail.com>
2021-03-17 20:42:53 +0300

    Fix commit

--- src/test/java/ru/netology/SeleniumTests.java
+++ src/test/java/ru/netology/SeleniumTests.java
@@ -21,7 +21,7 @@ public class SeleniumTests {
 
     @BeforeAll
     static void setUpAll() {
-        WebDriverManager.chromedriver().linux().setup();
+        WebDriverManager.chromedriver().setup();
 
     }
 
